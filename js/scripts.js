d71e2e3f50c93a2af121757bbfce54c6e133e253
Student <example@example.com>
2019-06-12 20:56:35 -0700

    do nothing until I fix the js file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,30 @@
 $(document).ready(function() {
-  $("#groceries form").submit(funtion(event) {
-    var blanks = ["itemOne", "itemTwo", "itemThree", "itemFour", "itemFive", "itemSix"];
+  $("#groceries form").submit(function(event) {
+    var items = ["itemOne", "itemTwo", "itemThree", "itemFour", "itemFive", "itemSix"];
+    console.log(items);
 
     groceries.forEach(function(grocery) {
-      var userInput = $("input#" + grocery).val();
-      $("." + grocery).text(userInput);
-    });
+     var userInput = $("input#" + blank).val();
+     $("." + blank).text(userInput);
+     });
 
-    $("#results").show();
-    $("#groceries").hide();
+    // var upperItems = items.map(function(item) {
+    //   return item.toUpperCase();
+    //   console.log(upperItems);
+    // });
+    //
+    // var alphaItems = upperItems(sort);
+    // console.log(alphaItems);
+    // });
+    //
+    // alphaItems.forEach(function(item) {
+    //   var groceries = $("input#" + blank).val();
+    //   $("." + blank).text(userInput);
+    //
+    // });
+    //
+    // $("#results").show();
+    // $("#groceries").hide();
 
     event.preventDefault();
   });
